4b2c2af04d98de50d5dddb6429940f77a1e906b6
Eduardo Jaime <eduardojaime86@gmail.com>
2020-08-10 18:23:54 -0400

    Console App Test

---
+++ 202008/ConsoleAppTest/ConsoleAppTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!");
+
+            // Keep console open until user presses any key
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
